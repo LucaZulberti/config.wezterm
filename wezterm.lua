70e9afffc8d3ccd12c41833f3200c405f291be52
Luca Zulberti <zulberti.luca@gmail.com>
2025-01-09 06:32:21 +0100

    :art: Indent comments correctly

--- wezterm.lua
+++ wezterm.lua
@@ -4,9 +4,9 @@ local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').moon
 
 -- and finally, return the configuration to wezterm
 return {
-    font = wezterm.font("0xProto Nerd Font"),           -- Set the main font
-    font_size = 12.0,                                   -- Adjust font size as needed
+    font = wezterm.font("0xProto Nerd Font"),             -- Set the main font
+    font_size = 12.0,                                     -- Adjust font size as needed
     harfbuzz_features = { "calt=1", "clig=1", "liga=1" }, -- Enable ligatures
     colors = theme.colors(),
-    window_frame = theme.window_frame(),                -- needed only if using fancy tab bar
+    window_frame = theme.window_frame(),                  -- needed only if using fancy tab bar
 }
